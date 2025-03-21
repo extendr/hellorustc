e491570a0ab713c0e1c6b42ce47184dd59f7758a
Mossa <cgmossa@gmail.com>
2025-03-21 16:27:19 +0100

    wp: attempting a full bindings inclusion

--- src/bindings.rs
+++ src/bindings.rs
@@ -6,17 +6,66 @@
 
 include!("bindings/R.rs");
 
-#[non_exhaustive]
-#[repr(transparent)]
-#[derive(Debug)]
-pub struct SEXPREC(std::ffi::c_void);
-
 // TODO: What should this be? libc::FILE?
 pub enum FILE {}
 
 include!("bindings/R_ext/Boolean.rs");
+include!("custom_bindings/custom_Boolean.rs");
+
+
+
+include!("custom_bindings/custom_Altrep.rs");
+include!("bindings/R_ext/Altrep.rs");
+include!("bindings/R_ext/Applic.rs");
+include!("bindings/R_ext/Arith.rs");
+include!("bindings/R_ext/BLAS.rs"); // FIXME: `zdrot_` is defined here
+// include!("bindings/R_ext/Lapack.rs"); // FIXME: `zdrot_` here too
+include!("bindings/R_ext/Linpack.rs");
+
+include!("bindings/R_ext/Callbacks.rs");
+
 include!("bindings/R_ext/Complex.rs");
+
+// include!("bindings/R_ext/Connections.rs"); // FIXME: missing va_list!?
+include!("bindings/R_ext/Constants.rs");
+include!("bindings/R_ext/Error.rs");
+// include!("bindings/R_ext/eventloop.rs"); // FIXME: missing fd_set??
+include!("bindings/R_ext/GetX11Image.rs");
+
+include!("bindings/R_ext/GraphicsDevice.rs");
+include!("bindings/R_ext/GraphicsEngine.rs");
+
+include!("bindings/R_ext/Itermacros.rs");
+include!("bindings/R_ext/libextern.rs");
+include!("bindings/R_ext/MathThreads.rs");
+include!("bindings/R_ext/Memory.rs");
+include!("bindings/R_ext/Parse.rs");
+// include!("bindings/R_ext/Print.rs");
+// include!("bindings/R_ext/PrtUtil.rs");
+// include!("bindings/R_ext/QuartzDevice.rs");
+// include!("bindings/R_ext/Rallocators.rs");
+// include!("bindings/R_ext/Random.rs");
 
 include!("bindings/R_ext/Rdynload.rs");
 include!("custom_bindings/custom_Rdynload.rs");
+
+// include!("bindings/R_ext/Riconv.rs");
+// include!("bindings/R_ext/RS.rs");
+// include!("bindings/R_ext/RStartup.rs");
+// include!("bindings/R_ext/stats_package.rs");
+// include!("bindings/R_ext/stats_stubs.rs");
+// include!("bindings/R_ext/Utils.rs");
+// include!("bindings/R_ext/Visibility.rs");
+
 include!("bindings/Rinternals.rs");
+include!("custom_bindings/custom_Rinternals.rs");
+
+include!("bindings/libintl.rs");
+include!("bindings/Rconfig.rs");
+include!("bindings/Rdefines.rs");
+// include!("bindings/Rembedded.rs"); // defined in R internals!
+// include!("bindings/Rinterface.rs");
+// include!("bindings/Rmath.rs"); // defined elements in R.rs
+include!("bindings/Rversion.rs"); // defined elements in R.rs
+
+
